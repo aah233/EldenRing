51316e8d6172db3a7b3b3bbef7c14a2f5d58243f
Antonio Aranda <antonio.aranda@alboradait.com>
2024-05-26 19:33:39 +0200

    Actualizo con la vista de los items

--- apache-php-elden/www/frontend/src/views/Tab1Page.vue
+++ apache-php-elden/www/frontend/src/views/Tab1Page.vue
@@ -2,28 +2,14 @@
   <ion-list>
     <!-- The reorder gesture is disabled by default, enable it to drag and drop items -->
     <ion-reorder-group :disabled="false" @ionItemReorder="handleReorder($event)">
-      <ion-item>
-        <ion-label> Item 1 </ion-label>
-        <ion-reorder slot="end"></ion-reorder>
-      </ion-item>
-
-      <ion-item>
-        <ion-label> Item 2 </ion-label>
-        <ion-reorder slot="end"></ion-reorder>
-      </ion-item>
-
-      <ion-item>
-        <ion-label> Item 3 </ion-label>
-        <ion-reorder slot="end"></ion-reorder>
-      </ion-item>
-
-      <ion-item>
-        <ion-label> Item 4 </ion-label>
-        <ion-reorder slot="end"></ion-reorder>
-      </ion-item>
-
-      <ion-item>
-        <ion-label> Item 5 </ion-label>
+      <ion-item v-for="(item, index) in items" :key="index">
+        <ion-thumbnail slot="start">
+          <img :src="item.image" />
+        </ion-thumbnail>
+        <ion-label>
+          <h2>{{ item.name }}</h2>
+          <p>{{ item.description }}</p>
+        </ion-label>
         <ion-reorder slot="end"></ion-reorder>
       </ion-item>
     </ion-reorder-group>
@@ -31,24 +17,46 @@
 </template>
 
 <script lang="ts">
-import { IonItem, IonLabel, IonList, IonReorder, IonReorderGroup } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonItem, IonLabel, IonList, IonReorder, IonReorderGroup, IonThumbnail } from '@ionic/vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
-  components: { IonItem, IonLabel, IonList, IonReorder, IonReorderGroup },
+  components: { IonItem, IonLabel, IonList, IonReorder, IonReorderGroup, IonThumbnail },
   setup() {
-    const handleReorder = (event: CustomEvent) => {
-      // The `from` and `to` properties contain the index of the item
-      // when the drag started and ended, respectively
-      console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+    const items = ref([]);
 
-      // Finish the reorder and position the item in the DOM based on
-      // where the gesture ended. This method can also be called directly
-      // by the reorder group
+    const fetchItems = async () => {
+      try {
+        const response = await axios.get('https://local-backend.elden.com/api/items');
+        items.value = response.data;
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    };
+
+    const handleReorder = (event: CustomEvent) => {
+      console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+      // Update the items array based on the reorder event
+      const movedItem = items.value.splice(event.detail.from, 1)[0];
+      items.value.splice(event.detail.to, 0, movedItem);
+
+      // Finish the reorder
       event.detail.complete();
     };
 
-    return { handleReorder };
+    onMounted(() => {
+      fetchItems();
+    });
+
+    return { items, handleReorder };
   },
 });
 </script>
+
+<style scoped>
+ion-thumbnail {
+  width: 60px;
+  height: 60px;
+}
+</style>
